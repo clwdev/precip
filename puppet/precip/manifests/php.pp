cc4fb8bdf293f1a819367e4c6ce56756c0e5fb7d
Jeff George <jeff.george@gmail.com>
2017-07-27 14:42:32 -0400

    Add Soap libraries, also reorder the dependencies so they're always 5.6 -> 7.0

--- puppet/precip/manifests/php.pp
+++ puppet/precip/manifests/php.pp
@@ -5,29 +5,6 @@ class precip::php {
   # Get Ondrej's PPA
   ::apt::ppa { "ppa:ondrej/php": }
   
-  # Get all the PHP 7 deps
-  package{[
-    'php7.0-cli',
-    'php7.0-common',
-    'php7.0-curl',
-    'php7.0-dev',
-    'php7.0-fpm',
-    'php7.0-gd',
-    'php7.0-intl',
-    'php7.0-json',
-    'php7.0-mbstring',
-    'php7.0-mcrypt',
-    'php7.0-mysql',
-    'php7.0-opcache',
-    'php7.0-readline',
-    'php7.0-sqlite3',
-    'php7.0-xml',
-    'php7.0-zip',
-    ]:
-    ensure  => present,
-    require => [Apt::Ppa['ppa:ondrej/php'], Class['apt::update']],
-  }
-
   # Get all the PHP 5.6 deps
   package{[
     'php5.6-cli',
@@ -43,9 +20,34 @@ class precip::php {
     'php5.6-mysql',
     'php5.6-opcache',
     'php5.6-readline',
+    'php5.6-soap',
     'php5.6-sqlite3',
     'php5.6-xml',
     'php5.6-zip',
+    ]:
+    ensure  => present,
+    require => [Apt::Ppa['ppa:ondrej/php'], Class['apt::update']],
+  }
+  
+  # Get all the PHP 7 deps
+  package{[
+    'php7.0-cli',
+    'php7.0-common',
+    'php7.0-curl',
+    'php7.0-dev',
+    'php7.0-fpm',
+    'php7.0-gd',
+    'php7.0-intl',
+    'php7.0-json',
+    'php7.0-mbstring',
+    'php7.0-mcrypt',
+    'php7.0-mysql',
+    'php7.0-opcache',
+    'php7.0-readline',
+    'php7.0-soap',
+    'php7.0-sqlite3',
+    'php7.0-xml',
+    'php7.0-zip',
     ]:
     ensure  => present,
     require => [Apt::Ppa['ppa:ondrej/php'], Class['apt::update']],
