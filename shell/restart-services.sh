af683e7f0662706dc55d660e5f3778e88ffd2a87
Christian Stück <christian.stuck@tuidev.com>
2017-09-19 15:02:20 -0400

    Added logic to recover the cachefilesd service if not started at boot.

--- shell/restart-services.sh
+++ shell/restart-services.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-# Restarts Apache and MySQL.
+
+# Restart Apache and MySQL.
 service apache2 restart
 service mysql restart
+
+# Start cachefilesd service.
+# Note: Two attempts are made to start the service,
+# as the first attempt may not succeed.
+if ! ps ax | grep -v grep | grep cachefilesd > /dev/null
+then
+    service cachefilesd start
+    if ! ps ax | grep -v grep | grep cachefilesd > /dev/null
+    then
+        service cachefilesd start
+    fi
+fi
